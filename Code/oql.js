74cba1dccee89c1c6f5759600e19954f77d0a79f
Jesse Dinkin <kaeon.ace@gmail.com>
2025-12-08 20:43:52 -0500

    Code Updated

--- Code/oql.js
+++ Code/oql.js
@@ -1,9 +1,16 @@
 let utils = {
+	left: /^[A-Za-z_$][\w$]*(?:\.[A-Za-z_$][\w$]*|\[(?:".*?"|'.*?'|\d+|[A-Za-z_$][\w$]*)\])*$/,
 	normalizeContext: (context) => {
 
 		context.access = context.access != null ? context.access : {};
 		context.operation = context.operation != null ? context.operation : {};
 		context.filters = context.filters != null ? context.filters : [];
+
+		context.operation.type =
+			context.operation.type != null ? context.operation.type : "read";
+
+		context.operation.data =
+			context.operation.data != null ? context.operation.data : [];
 
 		return context;
 	},
@@ -25,147 +32,155 @@ module.exports = [
 	{
 		process: (context, args) => {
 
-			if(context.local.operator != "access")
-				return null;
-
-			return JSON.stringify(utils.normalizeContext({
-				access: {
-					url: utils.normalizeValue(args[0]),
-					options: args[1] != null ?
-						JSON.parse("" + args[1]) :
-						null
-				},
-				operation: {
-					type: "read"
-				}
-			}));
+			return context.local.operator == "access" ?
+				JSON.stringify(utils.normalizeContext({
+					access: {
+						url: utils.normalizeValue(args[0]),
+						options: args[1] != null ?
+							JSON.parse("" + args[1]) :
+							null
+					}
+				})) :
+				null;
 		},
 		tags: ["oql", "access"]
 	},
 	{
 		process: (context, args) => {
 
-			if(context.local.operator != "append")
-				return null;
-
-			if(!args[0].startsWith("{"))
-				return null; // STUB
-
-			let data = utils.normalizeContext(JSON.parse(args[0]));
-
-			data.operation.type = "create"
-			data.operation.data = args.slice(1).map(item => JSON.parse(item));
-
-			return JSON.stringify(data);
+			return context.local.operator == "append" ?
+				`(Array.isArray(${
+					args[0]
+				})?([${
+					args.slice(1).map(item => `(${item})`).join(",")
+				}].forEach(item=>{(${
+					args[0]
+				}).push(item);}):((context)=>{
+					context.operation.type="create";
+				${
+					args.slice(1).map(
+						item => `context.operation.data.push(${item});`
+					).join("")
+				}return context;})(${
+					args[0]
+				}))` :
+				null;
 		},
 		tags: ["oql", "append"]
 	},
 	{
 		process: (context, args) => {
 
-			if(context.local.operator != "at")
-				return null;
-
-			if(!args[0].startsWith("{"))
-				return `${args[0]}[${args[1]}]`;
-
-			let data = utils.normalizeContext(JSON.parse(args[0]));
-
-			args.slice(1).forEach(item => {
-
-				data.filters.push({
-					type: "at",
-					options: {
-						value: utils.normalizeValue(item)
-					}
-				});
-			});
-
-			return JSON.stringify(data);
+			return context.local.operator == "at" ?
+				`(Array.isArray(${
+					args[0]
+				})?((${
+					args[0]
+				})${
+					args.slice(1).map(item => `[${item}]`).join("")
+				}):((context)=>{${
+					args.slice(1).map(item =>
+						`context.filters.push({type:"at",options:{value:(${
+							item
+						})}});`
+					).join("")
+				}return context;})(${
+					args[0]
+				}))` :
+				null;
 		},
 		tags: ["oql", "at"]
 	},
 	{
 		process: (context, args) => {
 
-			if(context.local.operator != "filter")
-				return null;
-
-			let data = utils.normalizeContext(JSON.parse(args[0]));
-
-			data.filters.push({
-				type: "filter",
-				options: {
-					value: context.local.list[2]
-				}
-			});
-
-			return JSON.stringify(data);
+			return context.local.operator == "filter" ?
+				`(((context)=>{context.filters.push(
+					{type:"filter",options:{value:(
+				${
+					JSON.stringify(context.local.list[2])
+				})}});return context;})(${args[0]}))` :
+				null;
 		},
 		tags: ["oql", "filter"]
 	},
 	{
 		process: (context, args) => {
 
-			if(context.local.operator != "remove")
-				return null;
-
-			if(!args[0].startsWith("{"))
-				return null; // STUB
-
-			let data = utils.normalizeContext(JSON.parse(args[0]));
-			data.operation.type = "delete"
-
-			return JSON.stringify(data);
+			return context.local.operator == "remove" ?
+				`(Array.isArray(${
+					args[0]
+				})?((${args[0]}).splice(${args[1], 1}))${
+					args.slice(1).map(item => `[${item}]`).join("")
+				}):(((context)=>{
+					context.operation.type="delete";return context;
+				})(${
+					args[0]
+				})))` :
+				null;
 		},
 		tags: ["oql", "remove"]
 	},
 	{
 		process: (context, args) => {
 
-			if(context.local.operator != "set")
-				return null;
-
-			if(!args[0].startsWith("{")) {
-
-				return context.state[args[0]] != null ?
-					`${args[0]}=context.state[${args[0]}];` :
-					`${
-						args[0]
-					}=${
-						args[1]
-					},context.state["${
-						args[0]
-					}"]=${
-						args[0]
-					};`
-			}
-
-			let data = utils.normalizeContext(JSON.parse(args[0]));
-
-			data.operation.type = "update"
-			data.operation.data = args.slice(1).map(item => JSON.parse(item));
-
-			return JSON.stringify(data);
+			return context.local.operator == "set" ?
+				`((((item)=>{
+					if(item==null)return true;
+					return !(typeof item =="object"&&!Array.isArray(item));
+				})(typeof (${
+					args[0]
+				})=="undefined"?null:(${
+					args[0]
+				})))?(()=>{${
+					context.state[args[0]] != null ?
+						`${args[0]}=context.state[${args[0]}];` :
+						""
+				}(1, eval)(${
+					JSON.stringify(
+						context.state[args[0]] == null ?
+							`${
+								args[0]
+							}=${
+								args[1]
+							};` :
+							""
+					)
+				});${
+					/^[a-zA-Z_$][a-zA-Z0-9_$]*$/.test(args[0]) ?
+						`context.state["${
+							args[0]
+						}"]=${
+							args[0]
+						};` :
+						""
+				}})():((context)=>{
+					context.operation.type="update";
+				${
+					args.slice(1).map(
+						item => `context.operation.data.push(${item});`
+					).join("")
+				}return context;})(${
+					args[0]
+				}))\n` :
+				null;
 		},
 		tags: ["oql", "set"]
 	},
 	{
 		process: (context, args) => {
 
-			if(context.local.operator != "sort")
-				return null;
-
-			let data = utils.normalizeContext(JSON.parse(args[0]));
-
-			data.filters.push({
-				type: "sort",
-				options: {
-					value: JSON.parse(args[1])
-				}
-			});
-
-			return JSON.stringify(data);
+			return context.local.operator == "sort" ?
+				`(Array.isArray(${
+					args[0]
+				})?((${args[0]}).sort()):(((context)=>{
+					context.filters.push({type:"at",options:{value:(${
+						args[1]
+					})}});return context;
+				})(${
+					args[0]
+				})))` :
+				null;
 		},
 		tags: ["oql", "sort"]
 	},
